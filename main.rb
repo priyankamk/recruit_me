24fce05cd6dd67096f543de1f9bfd87e124cbf30
Priyanka Mukund <priyanka@kunday.com>
2019-02-04 23:32:38 +1100

    added delete button and working

--- main.rb
+++ main.rb
@@ -30,4 +30,21 @@ post '/jobs' do
   redirect "/jobs/#{@job.id}"
 end
 
+get '/jobs/:id/edit' do
+  @job = Job.find(params[:id])
+  erb :edit_job
+end
+
+# show the updated job list
+put '/jobs/:id' do
+  @job = Job.find(company_name: params[:company_name], job_title: params[:job_title], about_company: params[:about_company], about_job: params[:about_job], location: params[:location])
+  @job.update
+  redirect "/jobs/#{params[:id]}"
+end
+
+delete '/jobs/:id' do
+  @job = Job.find(params[:id])
+  @job.destroy
+  redirect '/jobs'
+end
 
